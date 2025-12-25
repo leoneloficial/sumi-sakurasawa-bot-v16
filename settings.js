1ed5d973e07454b72dcac6e2c8d44f7fe04acdd1
Leonel <166152300+leoneloficial@users.noreply.github.com>
2025-12-25 18:33:00 -0400

    Actualizar settings.js

--- settings.js
+++ settings.js
@@ -21,7 +21,7 @@ global.dev = '© 𝗣𝗼𝘄𝗲𝗿𝗲𝗱 𝗯𝘆 𝗔𝗱𝗼, 𝗬𝗼�
 global.author = '❀ 𝗠𝗮𝗱𝗲 𝘄𝗶𝘁𝗵 𝗔𝗱𝗼, 𝗬𝗼𝘀𝘂𝗲'
 global.etiqueta = '𝗔𝗱𝗼, 𝗬𝗼𝘀𝘂𝗲'
 global.currency = '€ 𝖤𝖴𝖱𝖮𝖲'
-global.banner = "https://files.catbox.moe/tm96iw.jpg"
+global.banner = "https://files.catbox.moe/hqflu9.jpg"
 global.icono = "https://files.catbox.moe/tm96iw.jpg"
 global.catalogo = fs.readFileSync('./lib/catalogo.jpg')
 
